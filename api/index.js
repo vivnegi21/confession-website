067fb9d035c6659036358bd2bc1950bfa187cd26
vivnegi21 <vivnegi21@gmail.com>
2024-01-15 23:43:56 +0530

    cors

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(cors({
     
     // origin: "http://localhost:3000",
     // methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    // credentials: true,
+    credentials: true,
 }));
 app.use(cookieParser());
 app.get('/test', (req, res) => {
